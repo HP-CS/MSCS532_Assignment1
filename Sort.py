127da21ff06cbeeca87919771ece85f2b416731d
HP-CS <hpark30360@ucumberlands.edu>
2024-10-27 23:35:33 -0400

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -7,3 +7,8 @@ def insertion_sort(arr):
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+
+if __name__ == "__main__":
+    data = [5, 2, 4, 6, 1, 3]
+    insertion_sort(data)
+    print("Array:", data)
